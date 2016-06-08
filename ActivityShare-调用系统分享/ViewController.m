64df98973bb257db1088af0a27dfa7ce866b39b8
嵇明新 <493972613@qq.com>
2016-06-08 11:38:39 +0800

    通过UIActivityViewController实现调用系统分享，并且UIActivityViewController还可以自定义分享

--- ActivityShare-调用系统分享/ViewController.m
+++ ActivityShare-调用系统分享/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "ZSCustomActivity.h"
 
 @interface ViewController ()
 
@@ -22,6 +23,27 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+/**
+ *  调用系统分享
+ *  UIActivityViewController相比于UIDocumentInteractionController优势除了可以添加额外的自定义服务，它还提供了非常好的原生服务的定制化功能。我们可以完全根据自己的需求，控制UIActivityViewController提供的系统服务的显示，比如我不想展示AirDrop这个功能，而这点在UIDocumentInteractionController是做不到的。想做到这一点，就需要使用到UIActivityViewController提供的一个属性:
+ *  @param sender <#sender description#>
+ */
+-(IBAction)shareActivityAction:(UIButton *)sender{
+    
+    UIActivityViewController *activity = [[UIActivityViewController alloc] initWithActivityItems:@[@"系统分享"] applicationActivities:@[[[ZSCustomActivity alloc] init]]];
+    
+    //activity.excludedActivityTypes = @[UIActivityTypeAirDrop];
+    
+    UIPopoverPresentationController *popoveView = activity.popoverPresentationController;
+    if (popoveView) {
+        popoveView.sourceView = nil;
+        popoveView.permittedArrowDirections = UIPopoverArrowDirectionUp;
+    }
+    
+    [self presentViewController:activity animated:YES completion:nil];
+
 }
 
 @end
